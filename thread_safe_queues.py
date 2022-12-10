dfa69b91c1caa2ab295607978674020297465c17
unknown <christineilagan02@gmail.com>
2022-12-10 22:45:38 +0800

    The thread_safe_queues program is done and the code is clear and functioning

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -1,9 +1,11 @@
 import argparse
-from queue import LifoQueue, PriorityQueue, Queue
 import threading
-from random import choice, randint
-
+from dataclasses import dataclass, field
+from enum import IntEnum
 from itertools import zip_longest
+from queue import LifoQueue, PriorityQueue, Queue
+from random import choice, randint
+from time import sleep
 
 from rich.align import Align
 from rich.columns import Columns
@@ -11,11 +13,7 @@ from rich.console import Group
 from rich.live import Live
 from rich.panel import Panel
 
-QUEUE_TYPES = {
-    "fifo": Queue,
-    "lifo": LifoQueue,
-    "heap": PriorityQueue
-}
+QUEUE_TYPES = {"fifo": Queue, "lifo": LifoQueue, "heap": PriorityQueue}
 
 PRODUCTS = (
     ":balloon:",
@@ -35,6 +33,75 @@ PRODUCTS = (
     ":yo-yo:",
 )
 
+@dataclass(order=True)
+class Product:
+    priority: int
+    label: str = field(compare=False)
+
+    def __str__(self):
+        return self.label
+
+
+class Priority(IntEnum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+PRIORITIZED_PRODUCTS = (
+    Product(Priority.HIGH, ":1st_place_medal:"),
+    Product(Priority.MEDIUM, ":2nd_place_medal:"),
+    Product(Priority.LOW, ":3rd_place_medal:"),
+)
+
+class Worker(threading.Thread):
+    def __init__(self, speed, buffer):
+        super().__init__(daemon=True)
+        self.speed = speed
+        self.buffer = buffer
+        self.product = None
+        self.working = False
+        self.progress = 0
+        
+    @property
+    def state(self):
+        if self.working:
+            return f"{self.product} ({self.progress}%)"
+        return ":zzz: Idle"
+
+    def simulate_idle(self):
+        self.product = None
+        self.working = False
+        self.progress = 0
+        sleep(randint(1, 3))
+
+    def simulate_work(self):
+        self.working = True
+        self.progress = 0
+        delay = randint(1, 1 + 15 // self.speed)
+        for _ in range(100):
+            sleep(delay / 100)
+            self.progress += 1
+            
+class Producer(Worker):
+    def __init__(self, speed, buffer, products):
+        super().__init__(speed, buffer)
+        self.products = products
+
+    def run(self):
+        while True:
+            self.product = choice(self.products)
+            self.simulate_work()
+            self.buffer.put(self.product)
+            self.simulate_idle()
+            
+class Consumer(Worker):
+    def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
+            
 class View:
     def __init__(self, buffer, producers, consumers):
         self.buffer = buffer
@@ -44,7 +111,7 @@ class View:
     def animate(self):
         with Live(
             self.render(), screen=True, refresh_per_second=10
-        ) as live:
+            ) as live:
             while True:
                 live.update(self.render())
 
@@ -79,32 +146,29 @@ class View:
         padding = " " * int(29 / 100 * worker.progress)
         align = Align(
             padding + worker.state, align="left", vertical="middle"
-        )
+            )
         return Panel(align, height=5, title=title)
-
-class Producer(Worker):
-    def __init__(self, speed, buffer, products):
-        super().__init__(speed, buffer)
-        self.products = products
-
-    def run(self):
-        while True:
-            self.product = choice(self.products)
-            self.simulate_work()
-            self.buffer.put(self.product)
-            self.simulate_idle()
-            
-class Consumer(Worker):
-    def run(self):
-        while True:
-            self.product = self.buffer.get()
-            self.simulate_work()
-            self.buffer.task_done()
-            self.simulate_idle()
-
+ 
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
+    products = PRIORITIZED_PRODUCTS if args.queue == "heap" else PRODUCTS
+    producers = [
+        Producer(args.producer_speed, buffer, PRODUCTS)
+        for _ in range(args.producers)
+    ]
+    consumers = [
+        Consumer(args.consumer_speed, buffer) for _ in range(args.consumers)
+    ]
 
+    for producer in producers:
+        producer.start()
+
+    for consumer in consumers:
+        consumer.start()
+
+    view = View(buffer, producers, consumers)
+    view.animate()
+    
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
@@ -113,38 +177,9 @@ def parse_args():
     parser.add_argument("-ps", "--producer-speed", type=int, default=1)
     parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
     return parser.parse_args()
-
+               
 if __name__ == "__main__":
     try:
         main(parse_args())
     except KeyboardInterrupt:
         pass
-    
-class Worker(threading.Thread):
-    def __init__(self, speed, buffer):
-        super().__init__(daemon=True)
-        self.speed = speed
-        self.buffer = buffer
-        self.product = None
-        self.working = False
-        self.progress = 0
-        
-    @property
-    def state(self):
-        if self.working:
-            return f"{self.product} ({self.progress}%)"
-        return ":zzz: Idle"
-
-    def simulate_idle(self):
-        self.product = None
-        self.working = False
-        self.progress = 0
-        sleep(randint(1, 3))
-
-    def simulate_work(self):
-        self.working = True
-        self.progress = 0
-        delay = randint(1, 1 + 15 // self.speed)
-        for _ in range(100):
-            sleep(delay / 100)
-            self.progress += 1
